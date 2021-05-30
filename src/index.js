02cfb700e708500dfbb6685170c649735169e06a
kermitt PDX <bluefishbrowndirt@gmail.com>
2021-05-29 22:04:33 -0500

    checkout point'

--- src/index.js
+++ src/index.js
@@ -41,20 +41,46 @@ function EndPoint({ position, onDrag, onEnd }) {
     let [bindHover, hovered] = useHover()
     let bindDrag = useDrag(onDrag, onEnd)
     return (
-        <mesh position={position} {...bindDrag} {...bindHover}>
-            <sphereBufferGeometry args={[7.5, 16, 16]} />
-            <meshBasicMaterial color={hovered ? 'hotpink' : 'white'} />
-
-            {/* <group {...props} ref={group}> */}
-
+        <mesh position={position} {...bindDrag} {...bindHover} >
+            {/* <sphereBufferGeometry args={[7.5, 16, 16]} /> */}
+            <sphereGeometry attach="geometry" args={[6, 16, 16]} />
+            {/* <meshBasicMaterial color={hovered ? 'hotpink' : 'white'} /> */}
+            <meshStandardMaterial attach="material" color="white" transparent roughness={0.1} metalness={0.1} />
             <Text depthTest={false} material-toneMapped={false} {...textProps}>
-                GOAT FINCH SWIFT
-</Text>
-            {/* </group> */}
-
+                WHALEkfgsdkgksdfkskfkf kask
+            </Text>
         </mesh>
     )
 }
+
+function FillLight({ brightness, color }) { return (<rectAreaLight width={3} height={3} intensity={brightness} color={color} position={[2, 1, 4]} lookAt={[0, 0, 0]} penumbra={2} castShadow />); } function RimLight({ brightness, color }) { return (<rectAreaLight width={2} height={2} intensity={brightness} color={color} position={[1, 4, -2]} rotation={[0, 180, 0]} castShadow />); }
+
+function Light({ brightness, color }) { return (<rectAreaLight width={3} height={3} color={color} intensity={brightness} position={[-2, 0, 5]} lookAt={[0, 0, 0]} penumbra={1} castShadow />); }
+
+function KeyLight({ brightness, color }) {
+    return (
+        <rectAreaLight
+            width={3}
+            height={3}
+            color={color}
+            intensity={brightness}
+            position={[-2, 0, 5]}
+            lookAt={[0, 0, 0]}
+            penumbra={1}
+            castShadow
+        />
+    );
+}
+
+function GroundPlane() { return (<mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>      <planeBufferGeometry attach="geometry" args={[500, 500]} />      <meshStandardMaterial attach="material" color="white" />    </mesh>); }
+
+function BackDrop() { return (<mesh receiveShadow position={[0, -1, -5]}>      <planeBufferGeometry attach="geometry" args={[500, 500]} />      <meshStandardMaterial attach="material" color="white" />    </mesh>); }
+
+
+
+
+function Sphere() { return (<mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>      <sphereGeometry attach="geometry" args={[1, 16, 16]} />      <meshStandardMaterial attach="material" color="white" transparent roughness={0.1} metalness={0.1} />    </mesh>); }
+
 
 function Line({ defaultStart, defaultEnd }) {
     const [start, setStart] = useState(defaultStart)
@@ -92,8 +118,14 @@ function App() {
     return (
         <Canvas invalidateFrameloop orthographic camera={{ position: [0, 0, 500] }}>
             <Controls>
+                {/* <GroundPlane />
+                <BackDrop /> */}
+                {/* <KeyLight brightness={5.6} color="#ffbdf4" />      <FillLight brightness={2.6} color="#bdefff" />      <RimLight brightness={54} color="#fff" /> */}
+                {/* <Light brightness={10} color={"white"} /> */}
+                <GroundPlane />
                 <Line defaultStart={[-100, -100, 0]} defaultEnd={[0, 100, 0]} />
                 <Line defaultStart={[0, 100, 0]} defaultEnd={[100, -100, 0]} />
+
             </Controls>
         </Canvas>
     )
