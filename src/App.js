7be3447ab068914c894c54a61a671e8d8a191740
kermitt PDX <bluefishbrowndirt@gmail.com>
2021-08-15 09:39:30 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,63 +1,88 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import { OrbitControls, Stars } from "drei";
-import { Physics, usePlane, useBox } from "use-cannon";
-import "./styles.css";
+//https://codesandbox.io/s/orbit-controls-and-drag-1gkvp?from-embed
 
-function Box(props) {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-  return (
-    <mesh
-      onClick={() => {
-        api.velocity.set(0, 2, 0);
-      }}
-      ref={ref}
-      position={[0, 2, 0]}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  );
+import * as THREE from 'three'
+import ReactDOM from 'react-dom'
+import React, { Fragment, useRef, useEffect, useState, useCallback, useContext, useMemo } from 'react'
+import { Canvas, useThree } from 'react-three-fiber'
+import { OrbitControls } from '@react-three/drei/OrbitControls'
+import './styles.css'
+
+function useHover() {
+  const [hovered, setHover] = useState(false)
+  const hover = useCallback((e) => (e.stopPropagation(), setHover(true)), [])
+  const unhover = useCallback((e) => setHover(false), [])
+  return [{ onPointerOver: hover, onPointerOut: unhover }, hovered]
 }
 
-function Plane(props) {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
+function useDrag(onDrag, onEnd) {
+  const [active, setActive] = useState(false)
+  const [, toggle] = useContext(camContext)
+  const activeRef = useRef()
+  const down = useCallback((e) => (setActive(true), toggle(false), e.stopPropagation(), e.target.setPointerCapture(e.pointerId)), [toggle])
+  const up = useCallback((e) => (setActive(false), toggle(true), e.target.releasePointerCapture(e.pointerId), onEnd && onEnd()), [onEnd, toggle])
+  const move = useCallback((event) => activeRef.current && (event.stopPropagation(), onDrag(event.unprojectedPoint)), [onDrag])
+  useEffect(() => void (activeRef.current = active))
+  return { onPointerDown: down, onPointerUp: up, onPointerMove: move }
+}
+
+function EndPoint({ position, onDrag, onEnd }) {
+
+  //    console.log(" Hello world " + position)
+
+  let [bindHover, hovered] = useHover()
+  let bindDrag = useDrag(onDrag, onEnd)
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]}>
-      {/* <planeBufferGeometry attach="geometry" args={[100, 100]} />
-        <meshLambertMaterial attach="material" color="lightblue" /> */}
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshBasicMaterial
-        attach="material"
-        color="black"
-        opacity={0.5}
-        transparent
-      />
+    <mesh position={position} {...bindDrag} {...bindHover}>
+      <sphereBufferGeometry args={[7.5, 16, 16]} />
+      <meshBasicMaterial color={hovered ? 'hotpink' : 'white'} />
     </mesh>
-  );
+  )
+}
+// https://stackoverflow.com/questions/13055214/mouse-canvas-x-y-to-three-js-world-x-y-z
+function setLastPosition() {
+  console.log(" !!!  ")
+}
+
+function Line({ defaultStart }) {
+  const [pos, setPos] = useState(defaultStart)
+  //    const [lastPos, setLastPos] = useState(defaultStart) 
+  const updatePos = (vector) => {
+    // setStart(vector)
+    console.log(vector[0].toFixed(3) + " | " + vector[1].toFixed(3) + " | " + vector[2].toFixed(3))
+    setPos(vector)
+  }
+  return (
+    <Fragment>
+      <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray())} onEnd={(v) => console.log("The end")} />
+
+      {/* <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray()); }  /> */}
+      {/* <EndPoint position={start} onDrag={(v) => { setStart(v.toArray()); finch(v) }} onDragend={(v) => setLastPosition(end)} /> */}
+    </Fragment>
+
+  )
+}
+
+const camContext = React.createContext()
+function Controls({ children }) {
+  const { gl, camera } = useThree()
+  const api = useState(true)
+  return (
+    <Fragment>
+      <OrbitControls args={[camera, gl.domElement]} enableDamping enabled={api[0]} />
+      <camContext.Provider value={api}>{children}</camContext.Provider>
+    </Fragment>
+  )
 }
 
 export default function App() {
-  let boxes = [
-    <Box position={[5, 1, 0]} />,
-    <Box position={[10, 2, 0]} />,
-    <Box position={[20, 1, 0]} />
-
-  ]
-
-
   return (
-    <Canvas>
-      <OrbitControls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Physics>
-        {boxes}
-        <Plane />
-      </Physics>
+    <Canvas invalidateFrameloop orthographic camera={{ position: [0, 0, 500] }}>
+      <Controls>
+        {/* <Line defaultStart={[-100, -100, 0]} defaultEnd={[0, 100, 0]} />
+                <Line defaultStart={[0, 100, 0]} defaultEnd={[100, -100, 0]} /> */}
+        <Line defaultStart={[-100, -100, 0]} />
+        <Line defaultStart={[0, 100, 0]} />
+      </Controls>
     </Canvas>
-  );
+  )
 }
--- src/index.js
+++ src/index.js
@@ -6,85 +6,100 @@ import React, { Fragment, useRef, useEffect, useState, useCallback, useContext, 
 import { Canvas, useThree } from 'react-three-fiber'
 import { OrbitControls } from '@react-three/drei/OrbitControls'
 import './styles.css'
+// import App from './App.js';
 
-function useHover() {
-    const [hovered, setHover] = useState(false)
-    const hover = useCallback((e) => (e.stopPropagation(), setHover(true)), [])
-    const unhover = useCallback((e) => setHover(false), [])
-    return [{ onPointerOver: hover, onPointerOut: unhover }, hovered]
-}
 
-function useDrag(onDrag, onEnd) {
-    const [active, setActive] = useState(false)
-    const [, toggle] = useContext(camContext)
-    const activeRef = useRef()
-    const down = useCallback((e) => (setActive(true), toggle(false), e.stopPropagation(), e.target.setPointerCapture(e.pointerId)), [toggle])
-    const up = useCallback((e) => (setActive(false), toggle(true), e.target.releasePointerCapture(e.pointerId), onEnd && onEnd()), [onEnd, toggle])
-    const move = useCallback((event) => activeRef.current && (event.stopPropagation(), onDrag(event.unprojectedPoint)), [onDrag])
-    useEffect(() => void (activeRef.current = active))
-    return { onPointerDown: down, onPointerUp: up, onPointerMove: move }
-}
+import { Provider as ReduxProvider } from 'react-redux';
 
-function EndPoint({ position, onDrag, onEnd }) {
+import App from './App.js';
+// import store from './redux/index.js';
 
-    //    console.log(" Hello world " + position)
+// function useHover() {
+//     const [hovered, setHover] = useState(false)
+//     const hover = useCallback((e) => (e.stopPropagation(), setHover(true)), [])
+//     const unhover = useCallback((e) => setHover(false), [])
+//     return [{ onPointerOver: hover, onPointerOut: unhover }, hovered]
+// }
 
-    let [bindHover, hovered] = useHover()
-    let bindDrag = useDrag(onDrag, onEnd)
-    return (
-        <mesh position={position} {...bindDrag} {...bindHover}>
-            <sphereBufferGeometry args={[7.5, 16, 16]} />
-            <meshBasicMaterial color={hovered ? 'hotpink' : 'white'} />
-        </mesh>
-    )
-}
-// https://stackoverflow.com/questions/13055214/mouse-canvas-x-y-to-three-js-world-x-y-z
-function setLastPosition() {
-    console.log(" !!!  ")
-}
+// function useDrag(onDrag, onEnd) {
+//     const [active, setActive] = useState(false)
+//     const [, toggle] = useContext(camContext)
+//     const activeRef = useRef()
+//     const down = useCallback((e) => (setActive(true), toggle(false), e.stopPropagation(), e.target.setPointerCapture(e.pointerId)), [toggle])
+//     const up = useCallback((e) => (setActive(false), toggle(true), e.target.releasePointerCapture(e.pointerId), onEnd && onEnd()), [onEnd, toggle])
+//     const move = useCallback((event) => activeRef.current && (event.stopPropagation(), onDrag(event.unprojectedPoint)), [onDrag])
+//     useEffect(() => void (activeRef.current = active))
+//     return { onPointerDown: down, onPointerUp: up, onPointerMove: move }
+// }
 
-function Line({ defaultStart }) {
-    const [pos, setPos] = useState(defaultStart)
-    //    const [lastPos, setLastPos] = useState(defaultStart) 
-    const updatePos = (vector) => {
-        // setStart(vector)
-        console.log(vector[0].toFixed(3) + " | " + vector[1].toFixed(3) + " | " + vector[2].toFixed(3))
-        setPos(vector)
-    }
-    return (
-        <Fragment>
-            <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray())} onEnd={(v) => console.log("The end")} />
+// function EndPoint({ position, onDrag, onEnd }) {
 
-            {/* <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray()); }  /> */}
-            {/* <EndPoint position={start} onDrag={(v) => { setStart(v.toArray()); finch(v) }} onDragend={(v) => setLastPosition(end)} /> */}
-        </Fragment>
+//     //    console.log(" Hello world " + position)
 
-    )
-}
+//     let [bindHover, hovered] = useHover()
+//     let bindDrag = useDrag(onDrag, onEnd)
+//     return (
+//         <mesh position={position} {...bindDrag} {...bindHover}>
+//             <sphereBufferGeometry args={[7.5, 16, 16]} />
+//             <meshBasicMaterial color={hovered ? 'hotpink' : 'white'} />
+//         </mesh>
+//     )
+// }
+// // https://stackoverflow.com/questions/13055214/mouse-canvas-x-y-to-three-js-world-x-y-z
+// function setLastPosition() {
+//     console.log(" !!!  ")
+// }
 
-const camContext = React.createContext()
-function Controls({ children }) {
-    const { gl, camera } = useThree()
-    const api = useState(true)
-    return (
-        <Fragment>
-            <OrbitControls args={[camera, gl.domElement]} enableDamping enabled={api[0]} />
-            <camContext.Provider value={api}>{children}</camContext.Provider>
-        </Fragment>
-    )
-}
+// function Line({ defaultStart }) {
+//     const [pos, setPos] = useState(defaultStart)
+//     //    const [lastPos, setLastPos] = useState(defaultStart) 
+//     const updatePos = (vector) => {
+//         // setStart(vector)
+//         console.log(vector[0].toFixed(3) + " | " + vector[1].toFixed(3) + " | " + vector[2].toFixed(3))
+//         setPos(vector)
+//     }
+//     return (
+//         <Fragment>
+//             <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray())} onEnd={(v) => console.log("The end")} />
 
-function App() {
-    return (
-        <Canvas invalidateFrameloop orthographic camera={{ position: [0, 0, 500] }}>
-            <Controls>
-                {/* <Line defaultStart={[-100, -100, 0]} defaultEnd={[0, 100, 0]} />
-                <Line defaultStart={[0, 100, 0]} defaultEnd={[100, -100, 0]} /> */}
-                <Line defaultStart={[-100, -100, 0]} />
-                <Line defaultStart={[0, 100, 0]} />
-            </Controls>
-        </Canvas>
-    )
-}
+//             {/* <EndPoint position={pos} onDrag={(v) => updatePos(v.toArray()); }  /> */}
+//             {/* <EndPoint position={start} onDrag={(v) => { setStart(v.toArray()); finch(v) }} onDragend={(v) => setLastPosition(end)} /> */}
+//         </Fragment>
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//     )
+// }
+
+// const camContext = React.createContext()
+// function Controls({ children }) {
+//     const { gl, camera } = useThree()
+//     const api = useState(true)
+//     return (
+//         <Fragment>
+//             <OrbitControls args={[camera, gl.domElement]} enableDamping enabled={api[0]} />
+//             <camContext.Provider value={api}>{children}</camContext.Provider>
+//         </Fragment>
+//     )
+// }
+
+// function App() {
+//     return (
+//         <Canvas invalidateFrameloop orthographic camera={{ position: [0, 0, 500] }}>
+//             <Controls>
+//                 {/* <Line defaultStart={[-100, -100, 0]} defaultEnd={[0, 100, 0]} />
+//                 <Line defaultStart={[0, 100, 0]} defaultEnd={[100, -100, 0]} /> */}
+//                 <Line defaultStart={[-100, -100, 0]} />
+//                 <Line defaultStart={[0, 100, 0]} />
+//             </Controls>
+//         </Canvas>
+//     )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+
+ReactDOM.render(
+    // <ReduxProvider store={store}>
+    <App />,
+    // </ReduxProvider>,
+    document.getElementById('root')
+);
